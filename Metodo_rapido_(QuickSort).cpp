07f711f2425b1b9f7b59aa9110b9807ce54b033b
Jonathan-md <jonathanpercamamani@gmail.com>
2026-09-24 16:20:44 -0500

    Subiendo la funcion QuickSort

---
+++ Metodo_rapido_(QuickSort).cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int Reduce(int A[],int inicio,int final){
+	int izq,der,pos,cen,aux;
+	izq=inicio;
+	der=final;
+	pos=izq;
+	cen=1;
+	while(cen==1){
+		cen=0;
+		while(A[pos]<=A[der] && pos!=der){
+			der--;
+		}
+		if(pos!=der){
+			aux=A[pos];
+			A[pos]=A[der];
+			A[der]=aux;
+			pos=der;
+			while(A[pos]!=A[izq] && pos!=izq){
+				izq++;
+			}
+			if(pos!=izq){
+				aux=A[pos];
+				A[pos]=A[izq];
+				A[izq]=aux;
+				pos=izq;
+				cen=1;
+			}
+		}
+		if(pos-1>inicio){
+			Reduce(A,inicio,pos-1);
+		}
+		if(pos+1<final){
+			Reduce(A,pos+1,final);
+		}
+	}
+	return 0;
+}
+
+int QuickSort(int A[],int n){
+	return Reduce(A,1,n);
+}
