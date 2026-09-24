924939c90d36630d9c00d6391cc97af09cbb9ef0
Jonathan-md <jonathanpercamamani@gmail.com>
2026-09-24 16:37:45 -0500

    Errores corregidos: en el primer for se agrega i++ y en el segundo while dentro del for se le condiciona al arreglo A[j-k] de que sea mayor a aux

--- Metodo_de_Shell.cpp
+++ Metodo_de_Shell.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int ShellSort(intA[],int n){
-	int k,aux;
+int ShellSort(int A[],int n){
+	int k,aux,j;
 	k=n+1;
 	while(k>1){
-		k=k/2
-		for(int i=k+1;i<=n){
+		k=k/2;
+		for(int i=k+1;i<=n;i++){
 			aux=A[i];
 			j=i;
-			while(j-k>=1 && A[j-k]){
+			while(j-k>=1 && A[j-k]>aux){
 				A[j]=A[j-k];
 				j=j-k;
 			}
@@ -18,3 +18,18 @@ int ShellSort(intA[],int n){
 	}
 	return 0;
 }
+
+int main(){
+	int A[90],n;
+	
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>A[i];
+	}
+	ShellSort(A,n);
+	for(int i=1;i<=n;i++){
+		cout<<A[i]<<" ";
+	}
+	return 0;
+}
+
